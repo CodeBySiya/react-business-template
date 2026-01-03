410726195f200e8240f70b46adf4df27465a2f54
CodeBySiya <mesiyamattu@gmail.com>
2026-01-03 15:28:55 +0530

    add name

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ function Main() {
                 <h6 className='text-4xl font-bold'>Business with <br></br><span className='text-orange-500'>Technology</span></h6>
             </div>
             <div>
-                <p className=''>IT company that provides a seamless and intuitive experience for users. The design will focus on clear navigation, easy access to information</p>
+                <p className=''>IT siya company that provides a seamless and intuitive experience for users. The design will focus on clear navigation, easy access to information</p>
             </div>
              
         </div>
